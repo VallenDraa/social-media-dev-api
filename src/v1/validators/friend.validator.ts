63768c97de740aaad9cbb966141738b187340b12
VallenDraa <jestinevallendradwiputra@gmail.com>
2024-04-08 16:36:23 +0700

    fix: trim strings for friendRequestValidator

--- src/v1/validators/friend.validator.ts
+++ src/v1/validators/friend.validator.ts
@@ -2,8 +2,8 @@ import Joi from 'joi';
 import { paginateValidator } from './paginate.validator';
 
 export const friendRequestValidator = Joi.object({
-	userId: Joi.string().uuid().required(),
-	friendId: Joi.string().uuid().required(),
+	userId: Joi.string().trim().uuid().required(),
+	friendId: Joi.string().trim().uuid().required(),
 }).label('FriendRequest');
 
 export const searchFriendValidator = paginateValidator
